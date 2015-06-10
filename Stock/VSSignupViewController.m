e1779f0e24b0364f7b2b300f0679758dc846c646
w <w@lxv.io>
2015-06-10 15:16:26 -0500

    skins

--- Stock/VSSignupViewController.m
+++ Stock/VSSignupViewController.m
@@ -11,6 +11,7 @@
 #import "VSAllTracksViewController.h"
 #import "VSTokenViewController.h"
 #import "AppDelegate.h"
+@import QuartzCore;
 
 @interface VSSignupViewController ()
 
@@ -18,24 +19,32 @@
 
 @implementation VSSignupViewController
 
-@synthesize nameField, emailField, passwordField, passwordConfirmationField, signingUpUser;
+@synthesize nameField, emailField, passwordField, passwordConfirmationField;
+@synthesize signUpButton, hasAccountButton;
+@synthesize signingUpUser;
 
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    
+    self.signingUpUser = [NSMutableDictionary create:@"user"];
     if ([[[LXSession thisSession] user] unconfirmed]) {
         UIStoryboard* storyboard = [UIStoryboard storyboardWithName:@"MobileLogin" bundle:[NSBundle mainBundle]];
         VSTokenViewController* vc = [storyboard instantiateViewControllerWithIdentifier:@"tokenViewController"];
         [self.navigationController presentViewController:vc animated:YES completion:nil];
     } else {
-        self.signingUpUser = [NSMutableDictionary create:@"user"];
         [LXSession storeLocalUserKey:[self.signingUpUser localKey]];
     }
+    
     [self setupGestureRecognizer];
     [self setupNavigationBar];
+    [self setupTextFieldAppearances];
+    [self setupButtonAppearances];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
@@ -46,6 +55,46 @@
 - (void) setupNavigationBar
 {
     [self.navigationController setNavigationBarHidden:YES];
+}
+
+- (void) setupButtonAppearances
+{
+    [[self.signUpButton titleLabel] setFont:[UIFont fontWithName:@"SourceSansPro-Regular" size:20.0f]];
+    self.signUpButton.layer.cornerRadius = 4;
+    self.signUpButton.clipsToBounds = YES;
+    
+    [[self.hasAccountButton titleLabel] setFont:[UIFont fontWithName:@"SourceSansPro-Regular" size:16.0f]];
+}
+
+- (void) setupTextFieldAppearances
+{
+    [self addBottomBorderToField:self.nameField];
+    [self addBottomBorderToField:self.emailField];
+    [self addBottomBorderToField:self.passwordField];
+    [self addBottomBorderToField:self.passwordConfirmationField];
+    
+    [self setTintForField:self.nameField withPlaceholder:@"Name"];
+    [self setTintForField:self.emailField withPlaceholder:@"Company Email*"];
+    [self setTintForField:self.passwordField withPlaceholder:@"Password"];
+    [self setTintForField:self.passwordConfirmationField withPlaceholder:@"Confirm Password"];
+}
+
+- (void) addBottomBorderToField:(UITextField*)field
+{
+    CGFloat borderHeight = 1.0f;
+    
+    CALayer *bottomBorder1 = [CALayer layer];
+    bottomBorder1.frame = CGRectMake(0, field.frame.size.height-borderHeight, field.frame.size.width, 1.0f);
+    bottomBorder1.backgroundColor = [UIColor whiteColor].CGColor;
+    
+    [field.layer addSublayer:bottomBorder1];
+}
+
+- (void) setTintForField:(UITextField*)field withPlaceholder:(NSString*)placeholderText
+{
+    [field setTintColor:[UIColor whiteColor]];
+    field.attributedPlaceholder = [[NSAttributedString alloc] initWithString:placeholderText attributes:@{NSForegroundColorAttributeName: [UIColor colorWithRed:1 green:1 blue:1 alpha:0.5], NSFontAttributeName: [UIFont fontWithName:@"SourceSansPro-Regular" size:16.0f]}];
+    [field setFont:[UIFont fontWithName:@"SourceSansPro-Regular" size:16.5f]];
 }
 
 # pragma mark - Gestures
@@ -91,20 +140,19 @@
     return nil;
 }
 
-- (void) setAttributes
-{
-    [self.signingUpUser setObject:self.nameField.text forKey:@"name"];
-    [self.signingUpUser setObject:self.emailField.text forKey:@"email"];
-    [self.signingUpUser setObject:self.passwordField.text forKey:@"password"];
-    [self.signingUpUser setObject:self.passwordField.text forKey:@"password_confirmation"];
-}
-
 # pragma mark - Actions
 
 - (IBAction)signupAction:(id)sender
 {
     if ([self inputsVerified]) {
-        [self setAttributes];
+        
+        [self.signingUpUser setObject:self.nameField.text forKey:@"name"];
+        [self.signingUpUser setObject:self.emailField.text forKey:@"email"];
+        [self.signingUpUser setObject:self.passwordField.text forKey:@"password"];
+        [self.signingUpUser setObject:self.passwordField.text forKey:@"password_confirmation"];
+        
+        [self showHUDWithMessage:@"Registering..."];
+        
         [self.signingUpUser saveRemote:^(id responseObject){
             if ([responseObject objectForKey:@"user"]) {
                 self.signingUpUser = [[responseObject objectForKey:@"user"] cleanDictionary];
@@ -119,9 +167,12 @@
                 VSTokenViewController* vc = [storyboard instantiateViewControllerWithIdentifier:@"tokenViewController"];
                 [self.navigationController presentViewController:vc animated:YES completion:nil];
             }
+            [self hideHUD];
         }failure:^(NSError *error) {
+            [self hideHUD];
             [self showAlertWithText:@"Only certain companies are able to use Versed at this time. Contact your system administrator for more details." andTitle:@"Sorry, your company isn't using Versed."];
         }];
+        
     } else {
         [self showAlertWithText:[self errorMessage]];
     }
@@ -146,4 +197,41 @@
     UIAlertView *av = [[UIAlertView alloc] initWithTitle:title message:text delegate:self cancelButtonTitle:@"Okay" otherButtonTitles: nil];
     [av show];
 }
+
+
+# pragma mark text field delegate
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField
+{
+    if (textField.tag < 4) {
+        NSInteger nextTag = textField.tag + 1;
+        for (UITextField* field in [self.view subviews]) {
+            if ([field isKindOfClass:[UITextField class]] && [field tag] == nextTag) {
+                [field becomeFirstResponder];
+            }
+        }
+    } else {
+        [self signupAction:textField];
+    }
+    return NO;
+}
+
+
+# pragma mark hud delegate
+
+- (void) showHUDWithMessage:(NSString*) message
+{
+    hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.labelText = message;
+    [hud setColor:[UIColor colorWithRed:0 green:0.5333 blue:0.345 alpha:0.8]];
+    [hud setLabelFont:[UIFont fontWithName:@"SourceSansPro-Light" size:14.0f]];
+}
+
+- (void) hideHUD
+{
+    if (hud) {
+        [hud hide:YES];
+    }
+}
+
 @end
