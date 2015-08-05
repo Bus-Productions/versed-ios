791b6c19e88684231b55c5bfac7c3a2dbeee3bdb
w <w@lxv.io>
2015-08-05 08:34:24 -0500

    meow

--- Stock/VSPromptOptions.m
+++ Stock/VSPromptOptions.m
@@ -26,13 +26,11 @@
     
     [self setupViews];
     
-    [self.topButton setTitle:@"Get Versed" forState:UIControlStateNormal];
-    [self.bottomButton setTitle:@"Test Your Knowledge" forState:UIControlStateNormal];
+    [self.bottomButton setTitle:@"Get Versed" forState:UIControlStateNormal];
+    [self.topButton setTitle:@"Test Your Knowledge" forState:UIControlStateNormal];
     
-    [self.topLabel setText:@"Go straight to tracks to learn about the trends and forces shaping business."];
-    [self.bottomLabel setText:@"Find out how much you know by taking a quiz about key business topics."];
-    [self.topLabel setFont:[UIFont fontWithName:@"SourceSansPro-Light" size:16.0f]];
-    [self.bottomLabel setFont:[UIFont fontWithName:@"SourceSansPro-Light" size:16.0f]];
+    [self.bottomLabel setText:@"Go straight to tracks to learn about the trends and forces shaping business."];
+    [self.topLabel setText:@"Find out how much you know by taking a quiz about key business topics."];
 }
 
 - (void)viewDidLoad
@@ -43,17 +41,27 @@
 
 - (void) setupViews
 {
-    [[self.topButton layer] setBorderWidth:1.0f];
-    [[self.topButton layer] setBorderColor:[UIColor whiteColor].CGColor];
-    [[self.topButton titleLabel] setFont:[UIFont fontWithName:@"SourceSansPro-Regular" size:22.0f]];
-    [[self.topButton titleLabel] setTextColor:[UIColor whiteColor]];
-    [self.topButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.topLabel setFont:[UIFont fontWithName:@"SourceSansPro-Light" size:16.0f]];
     
-    [[self.bottomButton layer] setBorderWidth:1.0f];
-    [[self.bottomButton layer] setBorderColor:[UIColor whiteColor].CGColor];
+    [self.bottomLabel setFont:[UIFont fontWithName:@"SourceSansPro-Light" size:16.0f]];
+    
+    //[[self.topButton layer] setBorderWidth:1.0f];
+    //[[self.topButton layer] setBorderColor:[UIColor whiteColor].CGColor];
+    [self.topButton setBackgroundColor:[UIColor whiteColor]];
+    [[self.topButton titleLabel] setFont:[UIFont fontWithName:@"SourceSansPro-Regular" size:22.0f]];
+    //[[self.topButton titleLabel] setTextColor:[UIColor whiteColor]];
+    //[self.topButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.topButton.layer setCornerRadius:8.0f];
+    [self.topButton setClipsToBounds:YES];
+    
+    //[[self.bottomButton layer] setBorderWidth:1.0f];
+    //[[self.bottomButton layer] setBorderColor:[UIColor whiteColor].CGColor];
+    [self.bottomButton setBackgroundColor:[UIColor whiteColor]];
     [[self.bottomButton titleLabel] setFont:[UIFont fontWithName:@"SourceSansPro-Regular" size:22.0f]];
-    [[self.bottomButton titleLabel] setTextColor:[UIColor whiteColor]];
-    [self.bottomButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    //[[self.bottomButton titleLabel] setTextColor:[UIColor whiteColor]];
+    //[self.bottomButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.bottomButton.layer setCornerRadius:8.0f];
+    [self.bottomButton setClipsToBounds:YES];
 }
 
 
@@ -67,20 +75,19 @@
 - (IBAction)topAction:(id)sender
 {
     [self closeWindow:NO];
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"goToOption" object:nil userInfo:@{@"to":@"allTracks"}];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"goToOption" object:nil userInfo:@{@"to":@"quiz"}];
 }
 
 
 - (IBAction)bottomAction:(id)sender
 {
     [self closeWindow:NO];
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"goToOption" object:nil userInfo:@{@"to":@"quiz"}];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"goToOption" object:nil userInfo:@{@"to":@"allTracks"}];
 }
 
 - (void) closeWindow:(BOOL)animated
 {
     [self dismissViewControllerAnimated:animated completion:^(void){
-        
     }];
 }
 
