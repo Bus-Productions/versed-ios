68c825f863c369eebb0e6245d53ab9f93801e8b7
mcarthurgill <mcarthur.gill@gmail.com>
2015-09-08 10:22:02 -0500

    last_login on signup

--- Stock/VSPromptOptions.m
+++ Stock/VSPromptOptions.m
@@ -38,7 +38,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    //to assign last_login for users who have signed up the first time. 
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
+        [[LXServer shared] requestPath:[NSString stringWithFormat:@"users/%@.json", [[[LXSession thisSession] user] ID]] withMethod:@"GET" withParamaters:nil authType:@"none" success:nil failure:nil];
+    });
+
 }
 
 - (void) setupViews
